dad54b55dffe9abe3c10f669cda0a869d4afb50a
dtd11 <157472142+doantd11@users.noreply.github.com>
2026-08-10 12:49:40 +0700

    [DatDT][1308][remove background temp logic]

--- components/temp/BackgroundTemp.jsx
+++ components/temp/BackgroundTemp.jsx
@@ -1,29 +1,3 @@
-export default function BackgroundTemp({ image }) {
-  return (
-    <div
-      aria-hidden="true"
-      style={{
-        height: '100vh',
-        left: 0,
-        overflow: 'hidden',
-        pointerEvents: 'none',
-        position: 'fixed',
-        top: 0,
-        width: '100vw',
-        zIndex: 0,
-      }}
-    >
-      <img
-        alt=""
-        src={image}
-        style={{
-          position: 'absolute',
-          right: 0,
-          top: '-180vh',
-          transform: 'scale(10) rotate(45deg)',
-          transformOrigin: 'top right',
-        }}
-      />
-    </div>
-  );
+export default function BackgroundTemp() {
+  return null;
 }
