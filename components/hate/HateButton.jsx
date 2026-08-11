051b8c0a6bf1dae46d7d39ff866c77d60b89f346
dtd11 <157472142+doantd11@users.noreply.github.com>
2026-08-11 16:05:50 +0700

    .

--- components/hate/HateButton.jsx
+++ components/hate/HateButton.jsx
@@ -2,5 +2,5 @@ import ButtonTemp from '../temp/ButtonTemp';
 import hateImage from '../../images/sticker/Hate.png';
 
 export default function HateButton(props) {
-  return <ButtonTemp {...props} image={hateImage} label="I Hate You" />;
+  return <ButtonTemp {...props} image={hateImage} label="I Hate You Because" />;
 }
