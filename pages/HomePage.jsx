9fbe488d69dc336ce4cada11a400d6e7ef69355e
dtd11 <157472142+doantd11@users.noreply.github.com>
2026-08-11 07:37:39 +0700

    [DatDT][HomePage][set circle radius to 65vh]

--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -46,12 +46,12 @@ export default function HomePage() {
         .button-ring {
           border: 2px solid #2563eb;
           border-radius: 50%;
-          bottom: -80vh;
-          height: 160vh;
+          bottom: -65vh;
+          height: 130vh;
           left: calc(100% - 20px);
           position: absolute;
           transition: transform 600ms ease-out;
-          width: 160vh;
+          width: 130vh;
         }
 
         .circle-button {
